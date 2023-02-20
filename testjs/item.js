c23c4094a3b058bb482030744aee31b4fdb151eb
Nguyen khac dat <datkhac09@gmail.com>
2023-02-20 21:01:28 +0700

    Done

---
+++ testjs/item.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    animateOut: 'fadeOut',
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
